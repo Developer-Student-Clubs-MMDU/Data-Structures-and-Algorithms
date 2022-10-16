f713947879985b7cd60bad57d02f842a02058447
Tanisha Bansal <100759293+tanisha12j@users.noreply.github.com>
2022-10-16 19:18:32 +0530

    Update insertion_sort.cpp

--- insertion_sort.cpp
+++ insertion_sort.cpp
@@ -1,57 +1,77 @@
-/*Probelm statement :Sort the following array using inserion sort in Ologn complexity
-Input : 45 32 12 15 19
-Output: 12 15 19 32 45
+/*
+problem statement- Insertion sort using binary search 
 
-Approach
-1.iterate over loop from arr[1] to arr[N] over the array
-2.Comapre current element with its previous element
-3.if current element is smaller than previous then compare with previous to previous element
-4.move the greater element one position up ,basically swapping the data
+Input:  23 45 2 1 4
+Output: 1 2 4 23 45 
 
+Algo:
+
+1.Firstly calling the function insertion sort 
+2.In that ,iterate over the loop from secound till last element
+3.store the current element in key
+4.find positon of element using binary search ,element just greater than  a[i] between a[0] to a[i-1]
+5.suppose this element is present at some index say k
+6.shift all the element towards right from k to i-1.
 */
 
-#include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
   
-void insertionSort(int arr[], int n) 
+
+int binarySearch(int a[], int item,int low, int high)//using binary search to find the element 
 {
-	    
-    for(int i = 1; i<n; i++) {
-        int temp = arr[i];//storing secound element and comparing it with first 
-        int j = i-1;
-        for(; j>=0; j--) {
-            
-            if(arr[j] > temp) {
-                //shifting 
-                arr[j+1] = arr[j];
-            }
-            else { // will break 
-                break;
-            }
-            
-        }
-        //copy temp value
-        arr[j+1] = temp;  
-    } 
- }
-
-int main() 
-{ 
-    int n;
-	cin>>n;
-	int arr[n];
-	//input the elements in array 
-	for(int i=0;i<n;i++)
-	{
-		cin>>arr[i];
-	}
-
-    insertionSort(arr, n); //calling the sort function
-
-	for(int i=0;i<n;i++)
-	{
-		cout<<arr[i]<<" ";//calling the print out function 
-	}
+    if (high <= low)
+        
+        return (item > a[low]) ?(low + 1) : low;
   
-    return 0; 
-} 
+    int mid = (low + high) / 2;
+  
+    if (item == a[mid])
+        return mid + 1;
+  
+    if (item > a[mid])
+        return binarySearch(a, item, mid + 1, high);
+
+
+    return binarySearch(a, item, low,mid - 1);
+}
+  
+void insertionSort(int a[], int n)//sorting the array 
+{
+    int i, loc, j, k, key;
+  
+    for (i = 1; i < n; ++i)//iterating the loop from secound position till last position 
+    {
+        j = i - 1;//taking one element previous than current 
+        key = a[i];
+  
+    
+        loc = binarySearch(a,key, 0, j);//now we need to find location where key should be inserted
+          
+        while (j >= loc)//shifting  till we find correct position 
+        {
+            a[j + 1] = a[j];
+            j--;
+        }
+        a[j + 1] = key;
+    }
+}
+  
+int main()
+{
+    int n;
+    cin>>n;
+    int a[n];
+    for(int i=0;i<n;i++)
+    {
+        cin>>a[i];
+    }
+    insertionSort(a, n);
+  
+    cout <<"Sorted array: \n";
+    for (int i = 0; i < n; i++)
+        {
+            cout <<" "<< a[i];
+        }
+    return 0;
+}
