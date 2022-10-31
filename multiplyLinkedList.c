0314d8e2fd21efdb1c0cde5ed578ef05b9f424b6
Shubhi <shubhi.paul2021@vitstudent.ac.in>
2022-10-31 22:13:43 +0530

    docs: add explaination and approach

--- multiplyLinkedList.c
+++ multiplyLinkedList.c
@@ -1,13 +1,39 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct Node
+/*APPROACH :
+
+Input two linked lists.
+Traverse the two linked lists to generate the required numbers.
+Print the multiplied values of the two numbers.  
+Not storing the value of the result or creating new list , hence no extra space used.
+*/
+
+/*
+EXPLAINATION -
+
+1) Start
+2) Create two heads for each linked list
+3) Input the numbers in reverse order and insert at beginning of the linked list
+4) Set two variables num1 and num2 to 0
+5) Start traversing the linked list
+6) Add the value of first node of each linked list to respective variable
+7) From the second node, multiply the variable by 10
+   and also take modulus of this value
+   and then add the value of the node to this 
+   variable.
+8) Repeat step 6 until we reach the last node of the list.
+9) Print the multiplied values of the two numbers.
+10) End.
+*/
+
+typedef struct Node //create link list node
 {
     int data;
     struct Node *next;
 } node;
 
-void add_start(node **start, int val)
+void add_start(node **start, int val) //insert node at start to reduce traversal time
 {
     node *newnode, *temp;
     newnode = (node *)malloc(sizeof(node));
@@ -24,7 +50,7 @@ void add_start(node **start, int val)
     }
 }
 
-void display(node *start)
+void display(node *start) //display the linked list
 {
     node *temp;
     temp = start;
@@ -47,7 +73,7 @@ void display(node *start)
     }
 }
 
-void multiplyList(node *first, node *second)
+void multiplyList(node *first, node *second)  //function multiply the two list
 {
     int num1 = 0, num2 = 0;
     while (first || second)
@@ -55,13 +81,13 @@ void multiplyList(node *first, node *second)
 
         if (first)
         {
-            num1 = (num1)*10 + first->data;
+            num1 = (num1)*10 + first->data; //linked list converted into decimal number
             first = first->next;
         }
 
         if (second)
         {
-            num2 = (num2)*10 + second->data;
+            num2 = (num2)*10 + second->data; //linked list converted into decimal number
             second = second->next;
         }
     }
